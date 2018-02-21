803ea13758522de1a186cbb942cd0cef13080fd7
Yige <yigec3@illinois.edu>
2018-02-20 20:51:11 -0600

    ok

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -8,9 +8,14 @@
  * @see <a href="https://cs125.cs.illinois.edu/lab/8/">Lab 8 Description</a>
  */
 public class Bank {
-
+    /**
+     *
+     */
     public String bankName;
 
+    /**
+     *
+     */
     public Bank() {
         bankName = "Illini Bank";
     }
@@ -79,6 +84,9 @@ public class Bank {
          */
     }
 
+    /**
+     *
+     */
     public static int totalAccounts = 0;
     /**
      * Uses static variable to get number of bank accounts opened.
